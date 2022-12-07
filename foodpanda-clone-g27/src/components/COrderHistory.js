7f47b4903292f639882e5afe817e3eae88e3af05
mariamtauqeer <mariam.tauqeer72@gmail.com>
2022-12-07 23:47:01 +0500

    update password working

--- foodpanda-clone-g27/src/components/COrderHistory.js
+++ foodpanda-clone-g27/src/components/COrderHistory.js
@@ -60,20 +60,7 @@ export const COrderHistory = () => {
             </div>
             <div class="justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
               <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                <li>
-                  <button
-                    clbuttonss="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                    aria-current="page"
-                    onClick={goHome}
-                  >
-                    Home
-                  </button>
-                </li>
-                <li>
-                  <button class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                    Orders
-                  </button>
-                </li>
+
               </ul>
             </div>
           </div>
--- foodpanda-clone-g27/src/components/DetailChange.js
+++ foodpanda-clone-g27/src/components/DetailChange.js
@@ -8,18 +8,22 @@ export const DetailChange = () => {
   const [newpassword, setNewPassword] = useState("");
   const [updateStatus, setUpdateStatus] = useState([]);
 
-  const change = () => {
+  async function change (e) {
+    e.preventDefault();
     Axios.post("http://localhost:5000/updatepassword", {
       email: email,
       oldpassword: oldpassword,
       newpassword: newpassword,
     }).then((response) => {
-      console.log("below is response");
-      console.log(response);
-      console.log("below is response.data.message");
+      if(response.data.message === "Incorrect Old Password"){
+        alert("Incorrect Old Password");
+      }
+      else {
+        console.log(response.data.message);
+        setUpdateStatus([response.data.message]);
+        alert("Password Updated")
+      }
 
-      console.log(response.data.message);
-      setUpdateStatus([response.data.message]);
     });
   };
 
@@ -27,7 +31,7 @@ export const DetailChange = () => {
     <div className="change" class="flex flex-col items-center justify-center px-6 py-4 mx-auto md:h-screen lg:py-0 bg-gray-50 dark:bg-gray-900">
       <h1 class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">Change Details</h1>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <form class="p-6 space-y-4 md:space-y-6 sm:p-8" onSubmit={change}>
           <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Adress</label>
           <input
             type="email"
@@ -59,20 +63,12 @@ export const DetailChange = () => {
             }}
           />
           <button
-            onClick={change}
+
             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mt-7"
           >
             Change
           </button>
-
-          {updateStatus.map((obj) => {
-            return (
-              <div>
-                <h1>{obj}</h1>
-              </div>
-            );
-          })}
-        </div>
+        </form>
       </div>
     </div>
   );
